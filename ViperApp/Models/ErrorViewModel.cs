572c21088f811ae255d06c7b8d548ad8acde7003
ridam <ridam@LAPTOP-8LE72D2I>
2026-03-24 23:35:07 +0530

    Add project files.

---
+++ ViperApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViperApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
